7fa3326e8e9bb096f5e3cf97a0cda9d4d8475ea4
Paulo DAlberti <phdalberti@gmail.com>
2022-01-27 08:20:53 +0900

    Get variables for iframe dynamically

--- app/assets/javascripts/spree/frontend/solidus_acima.js
+++ app/assets/javascripts/spree/frontend/solidus_acima.js
@@ -1,8 +1,13 @@
 // Placeholder manifest file.
 // the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/frontend/all.js'
 
-const acima = new Acima.Client({
-  merchantId: "loca-0af305b8-23d6-4c60-9607-50a490872b0a",
-  iframeUrl: "https://ecom.sandbox.acimacredit.com",
-  iframeContainer: document.getElementById('my-container'),
-});
+document.addEventListener('DOMContentLoaded', async function () {
+  const iframeContainer = document.getElementById('my-container');
+  if (iframeContainer){
+    const acima = new Acima.Client({
+      merchantId: iframeContainer.dataset.merchantId,
+      iframeUrl: iframeContainer.dataset.iframeUrl,
+      iframeContainer: iframeContainer,
+    });
+  }
+})
