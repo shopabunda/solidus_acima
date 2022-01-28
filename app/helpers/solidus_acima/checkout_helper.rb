f6f419a33fd3b024222d622d32a206b07f5160d9
Paulo DAlberti <phdalberti@gmail.com>
2022-01-28 18:36:35 +0900

    Successfully display iframe, and make a user journey until the end

--- app/helpers/solidus_acima/checkout_helper.rb
+++ app/helpers/solidus_acima/checkout_helper.rb
@@ -2,5 +2,21 @@
 
 module SolidusAcima
   module CheckoutHelper
+    def order_for_acima_transaction(order)
+      {
+        id: order.number,
+        discounts: order.adjustment_total,
+        shipping: order.shipment_total,
+        salesTax: order.additional_tax_total,
+        lineItems: order.line_items.map do |line_item|
+          {
+            productId: line_item.sku,
+            productName: line_item.name,
+            unitPrice: line_item.price,
+            quantity: line_item.quantity
+          }
+        end
+      }.to_json
+    end
   end
 end
