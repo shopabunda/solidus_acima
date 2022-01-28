eae322d6ad181da1c9e8ec3232f194882dcc7b66
Paulo DAlberti <phdalberti@gmail.com>
2022-01-28 21:31:31 +0900

    Revert "Correctly import helper method"
    This reverts commit b5395271511efe6a08a7e467473fd2c8920285f0.

--- app/helpers/solidus_acima/checkout_helper.rb
+++ app/helpers/solidus_acima/checkout_helper.rb
@@ -2,21 +2,17 @@
 
 module SolidusAcima
   module CheckoutHelper
-    def cents(float)
-      (float * 100).to_i
-    end
-
     def order_for_acima_transaction(order)
       {
         id: order.number,
-        discounts: cents(order.adjustment_total),
-        shipping: cents(order.shipment_total),
-        salesTax: cents(order.additional_tax_total),
+        discounts: order.adjustment_total,
+        shipping: order.shipment_total,
+        salesTax: order.additional_tax_total,
         lineItems: order.line_items.map do |line_item|
           {
             productId: line_item.sku,
             productName: line_item.name,
-            unitPrice: cents(line_item.price),
+            unitPrice: line_item.price,
             quantity: line_item.quantity
           }
         end
